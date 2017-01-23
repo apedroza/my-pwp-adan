6defbd7b749709f1f6aeb529e96f44c9348ae3a6
Adam Pedroza <apedroza6@cnm.edu>
2017-01-23 09:34:16 -0700

    Complted up to Persons

---
+++ public_html/documentation/milestone-1.php
@@ -0,0 +1,49 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta charset="utf-8"/>
+
+		<title>Milestone 1- Adam PWP</title>
+
+	</head>
+	<body>
+		<header>
+			<h1 class="foo-heading">Roza Milestone 1</h1>
+		</header>
+			<h2 class="foo-heading">Purpose, Audience and Goal</h2>
+		<p id="description"><b>Purpose:</b>I hope to use this project as a showcase for my resume and whatever marketing creativity skill I can apply.</p>
+
+		<p id="description"><b>Audience:</b>The audience will consist of mostly professionals and employee prospects.</p>
+
+		<p id="description"><b>Purpose:</b>My main objective is to stand out with a prospective employer, internship, or someone seeking collabritve contract work in marketing or web design</p>
+
+
+
+		<h2 class="foo-heading"><b>Persona</b></h2>
+
+		<p id="description"><b>Name:</b>Rick James</p>
+
+		<p id="description"><b>Age:</b>45</p>
+
+		<p id="description"><b>Profession:</b>Rick has great idea for a start-up company, selling custome made boots. Since his father sold boots he is already familiar and confident with the
+		logistics, now he'e looking for someone to help him with the marketing and web development aspect.</p>
+
+		<p id="description"><b>Technology:</b>He sings in a band in his free time, keeping up with his fan base using an Android Samsung Galaxy 6s and an HP laptop which runs Windows 10.
+		His use of technology is often but narrow, he uses the same apps and sites everyday, and thats the extent of his use. </p>
+
+		<p id="description"><b>Behaviors and Habits:</b>Rick has had great business success in the past, which has allowed him to live somewhat financially free. He is constantly thinking business
+		concepts and usually is working on some kind of small project while continueing to play with his band. He maintains a pretty busy schedule as man with a lot of free personal time.</p>
+
+		<p id="description"><b>Needs:</b>Rick is well aware of strengths and has consistently had business partners to help him with technology and marketing ideas. His time management is well
+		rounded, working about 38-40 hours a week while filling most all of his free time with hobies and activities</p>
+
+
+
+
+		<h2 class="foo-heading"><b>Use Case</b></h2>
+
+		<p id="description"></p>
+
+
+	</body>
+</html>
