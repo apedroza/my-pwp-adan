a9f02db7114d961da623781740a60649f6315473
Adam Pedroza <apedroza6@cnm.edu>
2017-01-25 10:47:34 -0700

    Made corrections on <b> tags to <strong>

--- public_html/documentation/milestone-1.php
+++ public_html/documentation/milestone-1.php
@@ -3,6 +3,7 @@
 	<head>
 		<meta charset="utf-8"/>
 
+
 		<title>Roza</title>
 
 	</head>
@@ -11,30 +12,30 @@
 			<h1 class="foo-heading">Adan PWP- Milestone 1</h1>
 		</header>
 			<h2 class="foo-heading">Purpose, Audience and Goal</h2>
-		<p id="description"><b>Purpose:</b> I hope to use this project as a showcase for my resume and whatever creative marketing skills I can apply.</p>
+		<p id="description"><strong>Purpose:</strong> I hope to use this project as a showcase for my resume and whatever creative marketing skills I can apply.</p>
 
-		<p id="description"><b>Audience:</b> The audience will consist of mostly professionals and employee prospects.</p>
+		<p id="description"><strong>Audience:</strong> The audience will consist of mostly professionals and employee prospects.</p>
 
-		<p id="description"><b>Purpose:</b> The main objective is to stand out with prospective employers, internship opportunities or someone seeking contract work in marketing or web design</p>
+		<p id="description"><strong>Purpose:</strong> The main objective is to stand out with prospective employers, internship opportunities or someone seeking contract work in marketing or web design</p>
 
 
 
-		<h2 class="foo-heading"><b>Persona</b></h2>
+		<h2 class="foo-heading"><strong>Persona</strong></h2>
 
-		<p id="description"><b>Name:</b> Rick James</p>
+		<p id="description"><strong>Name:</strong> Rick James</p>
 
-		<p id="description"><b>Age:</b> 45</p>
+		<p id="description"><strong>Age:</strong> 45</p>
 
-		<p id="description"><b>Profession:</b> Rick has had great business success in the past, which has allowed him to live somewhat financially free. Rick is the lead singer in his band during his free time.
+		<p id="description"><strong>Profession:</strong> Rick has had great business success in the past, which has allowed him to live somewhat financially free. Rick is the lead singer in his band during his free time.
 			He is constantly thinking of business concepts and usually is working on some kind of small project while continueing to play with his band. He maintains a pretty busy schedule as man with a lot of free personal time.</p>
 
-		<p id="description"><b>Technology:</b> Rick keeps up with his fan base using an Android Samsung Galaxy 6s and an HP laptop which runs Windows 10.
+		<p id="description"><strong>Technology:</strong> Rick keeps up with his fan base using an Android Samsung Galaxy 6s and an HP laptop which runs Windows 10.
 		His use of technology is often but narrow, he uses the same apps and sites everyday, and thats the extent of his use. </p>
 
-		<p id="description"><b>Behaviors and Habits:</b>  Rick is well aware of his strengths and has consistently had business partners to help him with technology and marketing ideas.
+		<p id="description"><strong>Behaviors and Habits:</strong>  Rick is well aware of his strengths and has consistently had business partners to help him with technology and marketing ideas.
 			His time management is wellrounded, working about 38-40 hours a week while filling most all of his free time with hobies and activities</p>
 
-		<p id="description"><b>Needs:</b>  Rick has great idea for a new start-up company, designing and selling custome made boots. Since his father was also in the boot business, Rick is already familiar and confident with the
+		<p id="description"><strong>Needs:</strong>  Rick has great idea for a new start-up company, designing and selling custome made boots. Since his father was also in the boot business, Rick is already familiar and confident with the
 			logistics of the business. Now he'e looking for someone to help him with the marketing and web development aspect.</p>
 
 
@@ -44,13 +45,13 @@
 
 
 
-		<h2 class="foo-heading"><b>Use Case</b></h2>
+		<h2 class="foo-heading"><strong>Use Case</strong></h2>
 
 		<p id="description">As a single batchelor he eats out at least 8 times a week, usually it's for lunch. He often uses that time for
 		his business meeetings.</p>
 		<p id="description">I happen to meeet Rick during one of his lunch business meetings, and we begin talking about work and what we're currently
 		up to. Rick explains he is looking for someone to help him with the marketing and website design for his new business. After sharing a few ideas
-		with Rick I show him my "PWP", he continues to offer me an opportunity to work with him</p>
+		with Rick I show him my "PWP", he continues to offer me an opportunity to work with him.</p>
 
 	</body>
 </html>
