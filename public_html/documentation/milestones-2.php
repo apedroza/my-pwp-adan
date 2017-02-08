176df08e6b7ef1ee23f711f1cde1e9e837e0d47d
Adam Pedroza <apedroza6@cnm.edu>
2017-02-08 16:19:54 -0700

    Completed mobile mock-ups and content strategy for milestone2

---
+++ public_html/documentation/milestones-2.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta charset="utf-8"/>
+
+
+		<title>Roza</title>
+
+	</head>
+	<body>
+		<header>
+			<h1 class="foo-heading">Adan PWP- Milestone 2</h1>
+		</header>
+		<h2 class="foo-heading">Content Stategy</h2>
+		<p id="description">The objective of this site is to give prospective employers and professionals a place to view my resume and some current
+			information about myself, while implementing any creative marketing skills I'm capable of. It will be a simple one page abstract site
+			with 3 links that display my resume, an about me summery, and a contact page. The sites background will be one large image,
+			possibly with some animation.</p>
+
+		<h2 class="foo-heading">Wireframe</h2>
+
+
+		<img src="img/mobile.png" alt="mobile wireframe" width="700px">
+
+
+
+
+	</body>
+</html>
